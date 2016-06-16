1aa3f3d444dd32ca8374656f013740e2ee64169c
force <force@force-net.com>
2016-06-16 09:22:40 +0300

    added QuickLZ, bzip2 to benchmark, added block size benchmark, added some docs

--- Blazer.Benchmark/Program.cs
+++ Blazer.Benchmark/Program.cs
@@ -8,6 +8,9 @@ using Force.Blazer.Algorithms;
 using Force.Blazer.Algorithms.Crc32C;
 using Force.Blazer.Native;
 
+using ICSharpCode.SharpZipLib.BZip2;
+using ICSharpCode.SharpZipLib.GZip;
+
 using LZ4;
 
 using Snappy;
@@ -20,11 +23,12 @@ namespace Force.Blazer.Benchmark
 		{
 			BenchCrc32C();
 			//BenchNoCompression();
-			// BenchFile("Selesia Total", @"..\..\..\TestFiles\Silesia\ztotal.tar");
+			BenchFile("Selesia Total", @"..\..\..\TestFiles\Silesia\ztotal.tar");
 			// BenchFile("Log", @"..\..\..\TestFiles\Service.2016-05-01.log");
-			BenchFile("AdventureWorks (high compressible db)", @"..\..\..\TestFiles\AdventureWorks2012_Data.mdf");
-			BenchFile("enwiki8 (big text document)", @"..\..\..\TestFiles\enwik8");
+			// BenchFile("AdventureWorks (high compressible db)", @"..\..\..\TestFiles\AdventureWorks2012_Data.mdf");
+			// BenchFile("enwiki8 (big text document)", @"..\..\..\TestFiles\enwik8");
 			// BenchSilesia();
+			// BenchBlockSize(@"..\..\..\TestFiles\Service.2016-05-01.log");
 		}
 
 	    private static void BenchCrc32C()
@@ -104,6 +108,10 @@ namespace Force.Blazer.Benchmark
 			DoBench("LZ4/HC  ", array, x => new LZ4Stream(x, LZ4StreamMode.Compress, LZ4StreamFlags.HighCompression), x => new LZ4Stream(x, LZ4StreamMode.Decompress));
 			DoBench("Snappy  ", array, x => new SnappyStream(x, CompressionMode.Compress), x => new SnappyStream(x, CompressionMode.Decompress));
 			DoBench("StdGZip ", array, x => new GZipStream(x, CompressionMode.Compress), x => new GZipStream(x, CompressionMode.Decompress));
+			// very slow for usual running
+			// DoBench("BZip2    ", array, x => new BZip2OutputStream(x), x => new BZip2InputStream(x));
+			DoBenchQuickLZ("QuickLZ/1", 1, array);
+			DoBenchQuickLZ("QuickLZ/3", 3, array);
 		}
 
 		private static void DoBench(string title, byte[] data, Func<Stream, Stream> createCompressionStream, Func<Stream, Stream> createDecompressionStream)
@@ -113,6 +121,86 @@ namespace Force.Blazer.Benchmark
 			sw.Start();
 			using (var cs = createCompressionStream(ms))
 				new MemoryStream(data).CopyTo(cs);
+			var compressionTime = sw.ElapsedMilliseconds;
+
+			var comprArray = ms.ToArray();
+			sw.Restart();
+			var ds = createDecompressionStream(new MemoryStream(comprArray));
+			var ms2 = new MemoryStream(data.Length);
+			ds.CopyTo(ms2);
+			var decompressionTime = sw.ElapsedMilliseconds;
+			var resArray = ms2.ToArray();
+			if (!data.SequenceEqual(resArray))
+				Console.WriteLine("Data Integrity failed for " + title);
+			Console.WriteLine(
+				"{0}\t{1,4:0} MB/s\t{2,4:0} MB/s\t{3,7:0.000}%\t{4:0.000}",
+				title,
+				data.Length / (compressionTime / 1000.0) / 1048576,
+				data.Length / (decompressionTime / 1000.0) / 1048576,
+				100.0 * comprArray.Length / data.Length,
+				1.0 * data.Length / comprArray.Length);
+		}
+
+		private static void DoBenchQuickLZ(string title, int level, byte[] data)
+		{
+			var sw = new Stopwatch();
+			sw.Start();
+
+			var comprArray = QuickLZ.QuickLZ.compress(data, level);
+			var compressionTime = sw.ElapsedMilliseconds;
+
+			sw.Restart();
+			var resArray = QuickLZ.QuickLZ.decompress(comprArray);
+			var decompressionTime = sw.ElapsedMilliseconds;
+			
+			if (!data.SequenceEqual(resArray))
+				Console.WriteLine("Data Integrity failed for " + title);
+			Console.WriteLine(
+				"{0}\t{1,4:0} MB/s\t{2,4:0} MB/s\t{3,7:0.000}%\t{4:0.000}",
+				title,
+				data.Length / (compressionTime / 1000.0) / 1048576,
+				data.Length / (decompressionTime / 1000.0) / 1048576,
+				100.0 * comprArray.Length / data.Length,
+				1.0 * data.Length / comprArray.Length);
+		}
+
+		private static void BenchBlockSize(string fileName)
+		{
+			var array = File.ReadAllBytes(fileName);
+			var blockSize = 16;
+			while (blockSize < 1 << 21)
+			{
+				Console.WriteLine(blockSize);
+				DoBenchBlock("Stream   ", blockSize, array, x => new BlazerStreamCompressionStream(x, BlazerFlags.DefaultBlock), x => new BlazerDecompressionStream(x));
+				DoBenchBlock("Stream/H ", blockSize, array, x => new BlazerBaseCompressionStream(x, new StreamEncoderHigh(), BlazerFlags.DefaultBlock), x => new BlazerDecompressionStream(x));
+				DoBenchBlock("Block    ", blockSize, array, x => new BlazerBlockCompressionStream(x), x => new BlazerDecompressionStream(x));
+				DoBenchBlock("LZ4      ", blockSize, array, x => new LZ4Stream(x, LZ4StreamMode.Compress), x => new LZ4Stream(x, LZ4StreamMode.Decompress));
+				DoBenchBlock("LZ4/HC   ", blockSize, array, x => new LZ4Stream(x, LZ4StreamMode.Compress, LZ4StreamFlags.HighCompression), x => new LZ4Stream(x, LZ4StreamMode.Decompress));
+				DoBenchBlock("Snappy   ", blockSize, array, x => new SnappyStream(x, CompressionMode.Compress), x => new SnappyStream(x, CompressionMode.Decompress));
+				DoBenchBlock("SharpZip ", blockSize, array, x => new GZipOutputStream(x), x => new GZipInputStream(x));
+				blockSize <<= 1;
+			}
+		}
+
+		private static void DoBenchBlock(
+			string title,
+			int blockSize,
+			byte[] data,
+			Func<Stream, Stream> createCompressionStream,
+			Func<Stream, Stream> createDecompressionStream)
+		{
+			var ms = new MemoryStream();
+			var sw = new Stopwatch();
+			sw.Start();
+			using (var cs = createCompressionStream(ms))
+			{
+				for (var i = 0; i < data.Length; i += blockSize)
+				{
+					cs.Write(data, i, Math.Min(data.Length - i, blockSize));
+					cs.Flush();
+				}
+			}
+
 			var compressionTime = sw.ElapsedMilliseconds;
 
 			var comprArray = ms.ToArray();
--- README.md
+++ README.md
@@ -3,6 +3,59 @@ Blazer is high-performance archiver for .NET
 
 Currently, it is on develop stage. It is relatively stable, but format and structure has not finished yet. So, you can use it for testing or for streaming transfer (without storing sensitive data).
 
+## Description
+
+Blazer is low-compression, fast-speed archiver/compressor **with stream supporting**.
+In general, it similar to [LZ4](http://www.lz4.org/) or [Snapy](http://google.github.io/snappy/), but Blazer compression rate is slightly better and it supports streaming mode.
+
+What is streaming mode? Imagine, you have a monitoring service, which sends a lot of *similar* data about own status. Usually this data is same, but slightly differs by time and some parameters. But every portion of data requires a *flush* operation. In another words, it should be saved on disk or transferred via network.
+You want to compress this data, and want to do it fast, so, GZip is very slow for you. You can choose Snappy or LZ4, but these compressors are *block*. Every piece of data compressed independently. It is not problem with large chunks, but small chunks cause serious degradation.
+This pictore shows compression rate in relation to block size. Tests was performed on large log file, with .NET implemenation of corresponding compressors. In .NET, standard [GZipStream](https://msdn.microsoft.com/en-us/library/system.io.compression.gzipstream(v=vs.110).aspx) does not support flush at all, so, instead of it [SharpZipLib](https://icsharpcode.github.io/SharpZipLib/) was used.
+![Block Size Chart](Doc/Images/chart_blocksize1.png)
+
+*Percents of compression more than 100% due adding of headers, footers and other structural elements by compressors*
+
+You can see, that LZ4 and Snappy do very bad compression on small blocks, Blazer is better and GZip is best, but it is really slower other competitors. So, Blazer is good variant for fast data transferring and you can use it for every pipe streams (one side: writes and compress, other side: reads and decompress data).
+
+### Compressors/Encoders
+In real, Blazer can use **two** different algoritms with code names **stream** and **block** (There are another algorithm **no compression** it can be used for keeping blazer-structured stream).
+
+**Stream** algorithm described above and it is very good for compression of streamed data. 
+**Block** algorithm is used for comperssing files. It uses large independend chunks, gives better compression rate and better compression speed. Also, can be realized in multi-threaded mode (currently, not implemented). But it has average decompression speed (same as compression) and will give bad results for small chunks.
+
+Also, stream algorithm has **High** version (like LZ4 HC), which increases compression rate but compression speed is very low. This algorithm does not finished, it results even can be better in future implementations (but in fully compatible with standard structure, so, decompession is same).
+
+In another words, you can meet next situations:
+
+* **Compress-Decompress** (pipes) - use stream mode
+* **Compress and forget** (e.g. dumps and backups - use block mode, and if you have a time, zip, 7z, bzip2 or similar archivers
+* **Compress one time, decompress many times** (e.g. resources for application) - stream mode with high compression
+
+### Compression rates
+
+Compression rate of [Silesia corpus](http://sun.aei.polsl.pl/~sdeor/index.php?page=silesia) all tar'ed files.
+![Block Size Chart](Doc/Images/chart_comprrate1.png)
+
+*(Used .NET implemantations of corresponding algorithms, [QuickLZ](http://www.quicklz.com/) does not use stream implementation in C#, so, data was simulated by code)*
+
+Table version:
+
+
+Name                | Compression Rate (lower is better)
+--------------------|--------
+Blazer Stream       | 42.742%
+Blazer Stream High  | 37.608%
+Blazer Block        | 40.974%
+LZ4                 | 48.028%
+LZ4 HC              | 36.990%
+QuickLZ/1           | 44.691%
+QuickLZ/3           | 38.605%
+Snappy              | 47.847%
+GZip                | 32.191%
+BZip2               | 25.757%
+
+*Other details about compession/decompression speed will be described later)*
+
 ## Other important points
 
 Will be here later...
